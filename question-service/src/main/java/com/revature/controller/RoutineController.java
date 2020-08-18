95d52411032be588d1d8a6aa2f06d38c072b9f25
Dan Brown <hereisdanbrown@gmail.com>
2020-08-18 12:46:02 -0600

    Able to add a question to a routine.

--- question-service/src/main/java/com/revature/controller/RoutineController.java
+++ question-service/src/main/java/com/revature/controller/RoutineController.java
@@ -1,13 +1,12 @@
 package com.revature.controller;
 
+import com.revature.dao.QuestionDao;
 import com.revature.dao.RoutineDao;
+import com.revature.model.QuestionModel;
 import com.revature.model.RoutineModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,6 +17,9 @@ public class RoutineController {
 
     @Autowired
     RoutineDao routineDao;
+
+    @Autowired
+    QuestionDao questionDao;
 
     @GetMapping
     public ResponseEntity<List<RoutineModel>> getAllRoutines() {
@@ -33,5 +35,58 @@ public class RoutineController {
         Optional<RoutineModel> optional = routineDao.findById(id);
 
         return optional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.noContent().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<RoutineModel> insert(@RequestBody RoutineModel routine) {
+        int id = routine.getId();
+
+        if (id != 0) return ResponseEntity.badRequest().build();
+
+        routineDao.save(routine);
+        return ResponseEntity.status(201).body(routine);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<RoutineModel> delete(@PathVariable("id") int id) {
+        Optional<RoutineModel> optional = routineDao.findById(id);
+        if (optional.isPresent()) {
+            routineDao.delete(optional.get());
+            return ResponseEntity.accepted().body(optional.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+//
+//    // I need to add a mapping to get questions from a routine
+//    @GetMapping("/{id}/questions")
+//    public ResponseEntity<List<QuestionModel>>
+
+    @PutMapping("/{id}")
+    public ResponseEntity<RoutineModel> addQuestionToRoutine(@PathVariable("id") int id, @RequestBody int questionId) {
+        // Check to see if routine in database
+        Optional<RoutineModel> optionalR = routineDao.findById(id);
+        if (!optionalR.isPresent()) return ResponseEntity.notFound().build();
+        // Check to see if question in database
+        Optional<QuestionModel> optionalQ = questionDao.findById(questionId);
+        if (optionalQ.isPresent()) {
+            // Check to see if question is already in routine
+            if (optionalR.get().getQuestions().contains(optionalQ.get())) {
+                // Might have to override the isEquals() method
+                return ResponseEntity.badRequest().build();
+            } else {
+                // Add the question
+
+                System.out.println("Error after here?");
+                routineDao.addQuestionToRoutine(questionId, id);
+        System.out.println("Error before here?");
+                return ResponseEntity.ok(optionalR.get());
+
+            }
+
+        }
+        return ResponseEntity.badRequest().build();
+
+
     }
 }
--- question-service/src/main/java/com/revature/dao/RoutineDao.java
+++ question-service/src/main/java/com/revature/dao/RoutineDao.java
@@ -2,6 +2,34 @@ package com.revature.dao;
 
 import com.revature.model.RoutineModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
+
+//@Configuration
+//@EnableTransactionManagement
+//@Repository
+//@EnableJpaRepositories
 public interface RoutineDao extends JpaRepository<RoutineModel, Integer> {
+
+    @Modifying
+    @Transactional
+    @Query(
+            nativeQuery = true,
+            value = "INSERT INTO msa_example.question_routine(question_id, routine_id) values(:question_id, :routine_id)"
+    )
+    public void addQuestionToRoutine(@Param("question_id") int questionId, @Param("routine_id") int routineId);
+
+//    @PersistenceContext
+//    private EntityManager entityManager;
+//
+//    @Transactional
+//    public void addQuestionToRoutine(@Param("question_id") int questionId, @Param("routine_id") int routineId) {
+//      entityManager.createNativeQuery("INSERT INTO msa_example.question_routine(question_id, routine_id) values(:question_id, :routine_id)")
+//        .setParameter("question_id", questionId)
+//        .setParameter("routine_id", routineId)
+//        .executeUpdate();
+//    }
 }
